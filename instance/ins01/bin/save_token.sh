3977036bda18194a5cb1c296b7bc62430e845df4
Shohei Kakei <shok1122@zohomail.com>
2020-11-18 05:18:46 +0000

    Load conf file

--- instance/ins01/bin/save_token.sh
+++ instance/ins01/bin/save_token.sh
@@ -1,14 +1,30 @@
-access_token=$(curl "http://xkeycloak:8080/auth/realms/srv01/protocol/openid-connect/token" \
+user_id="$1"
+client_id="$2"
+realm="$3"
+
+user_pass=$(yq r conf.d/${user_id}.yaml password)
+client_secret=$(yq r conf.d/${user_id}.yaml ${client_id}.secret)
+
+echo '================================================================='
+echo " -  user_id:       $user_id"
+echo " -  user_pass:     $user_pass"
+echo " -  cliend_id:     $client_id"
+echo " -  client_secret: $client_secret"
+echo " -  realm:         $realm"
+echo '================================================================='
+
+access_token=$(curl "http://xkeycloak:8080/auth/realms/${realm}/protocol/openid-connect/token" \
     -s \
     -X POST \
     -d "grant_type=password" \
-    -d "client_id=app01" \
-    -d "client_secret=13d18d5a-4b2d-4f02-964e-9a6b9a5d3bdb" \
-    -d "username=user01" \
-    -d "password=pass" \
+    -d "client_id=${client_id}" \
+    -d "client_secret=${client_secret}" \
+    -d "username=${user_id}" \
+    -d "password=${user_pass}" \
     -d "scope=openid" \
     | jq .access_token \
     | awk -F \" '{print $2}')
 
-echo $access_token > /tmp/access_token
+echo $access_token
+echo $access_token > /tmp/${user_id}.${client_id}.${realm}.access_token
 
--- instance/ins01/bin/test_api.sh
+++ instance/ins01/bin/test_api.sh
@@ -1,5 +1,16 @@
+user_id="$1"
+client_id="$2"
+realm="$3"
+path="$4"
 
-access_token=$(cat /tmp/access_token)
+echo '================================================================='
+echo " -  user_id:   $user_id"
+echo " -  cliend_id: $client_id"
+echo " -  realm:     $realm"
+echo " -  path:      $path"
+echo '================================================================='
 
-curl -H "Authorization: Bearer $access_token" http://xkeycloak/srv01/
+access_token=$(cat /tmp/${user_id}.${client_id}.${realm}.access_token)
+
+curl -H "Authorization: Bearer $access_token" http://xkeycloak/${path}
 
