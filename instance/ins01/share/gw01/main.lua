9fcde7412c3b817fcc7ff4ef62ae72ac979a3c93
Shohei Kakei <shok1122@zohomail.com>
2020-11-24 13:00:06 +0000

    Implemented token inspection

--- instance/ins01/share/gw01/main.lua
+++ instance/ins01/share/gw01/main.lua
@@ -10,7 +10,11 @@ print(">>> access_token: " .. access_token)
 
 -- local res = ngx.location.capture('/auth_check', { vars = { access_token = access_token } })
 -- local res = ngx.location.capture('/auth_check', {copy_all_vars = true})
-local res = ngx.location.capture('/auth_check', {args = {token = access_token, name = "kakei"}})
+-- local res = ngx.location.capture('/auth_check', {args = {token = access_token}})
+local res = ngx.location.capture(
+        '/auth_check',
+        { body = "token="..access_token.."&token_type_hint=access_token&client_id=gw01&client_secret=32ea6553-3b43-420d-8272-8188a84c4642" }
+    )
 
 print(">>> status: " .. res.status)
 print(">>> body: " .. res.body)
